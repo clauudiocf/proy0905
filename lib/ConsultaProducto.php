55d1b9f77d554d159ca1e0812c6007bb576566ee
cetecom <cetecom@LC08L5712>
2017-05-09 11:26:56 -0300

    nuevo proyecto 3

--- lib/ConsultaProducto.php
+++ lib/ConsultaProducto.php
@@ -1,1 +1,23 @@
-<?php
+<?php 
+
+class ConsultaProducto{
+    
+private function Conexion(){
+    $miconn=new mysqli("localhost", "root", "avaras08", "ventas");
+        if($miconn->connect_errno){
+            return "Fallo al conectar a MySQL: (" .$miconn->connect_errno.")".$miconn->connect_errno;
+        }
+        return $miconn;
+    }
+    public function Lista(){
+        $sql = "SELECT * FROM Productos";
+        $resultado = $this->Conexion()->query($sql);
+        
+        $i = 0;
+        while($fila = $resultado->fetch_assoc()){
+            $oProducto= new Producto($fila("nombre"),$fila("precio"),$fila("codigo"));
+            $aProducto[$i]=$oProducto;
+        }
+        return $aProducto;
+    }
+}
