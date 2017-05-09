a1f1df1b7533cb9ba57bd99d1e53f5342b33cce7
cetecom <cetecom@LC08L5712>
2017-05-09 11:27:15 -0300

    nuevo proyecto 3

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+include('lib/Producto.php');
+include('lib/ConsultaProducto.php');
+$oProducto1=new Producto();
+$oProducto1->nombre="Nuevo Producto";
+$oProducto1->codigo="001";
+$oProducto2=new Producto();
+$oProducto2->nombre="Nuevo Producto 002";
+$oProducto2->codigo="002";
+$oProducto3=new Producto("Nuevo Producto 003",200.000,"003");
+
+$aProductos[0]=$oProducto1;
+$aProductos[1]=$oProducto2;
+$aProductos[2]=$oProducto3;
+?>
+
 <!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
@@ -11,7 +27,11 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        $aProductos = new ConsultaProducto();
+        foreach ($aProductos2 as $oPro){
+            echo $oPro->codigo." ".$oPro->nombre."/".$oPro->precio."/".$oPro->totalUSD();
+            echo "<br>";
+        }
         ?>
     </body>
 </html>
